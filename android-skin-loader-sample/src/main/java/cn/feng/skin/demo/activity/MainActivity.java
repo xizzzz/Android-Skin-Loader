07ab8b12a8e78251793146c406a143bc49cb3e00
Xi <xiz13@uci.edu>
2016-11-05 15:27:59 -0700

    call dynamic method

--- android-skin-loader-sample/src/main/java/cn/feng/skin/demo/activity/MainActivity.java
+++ android-skin-loader-sample/src/main/java/cn/feng/skin/demo/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.feng.skin.demo.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +15,7 @@ import java.lang.reflect.Method;
 import cn.feng.skin.demo.R;
 import cn.feng.skin.demo.fragment.ArticleListFragment;
 import cn.feng.skin.manager.base.BaseFragmentActivity;
+import cn.feng.skin.manager.util.ObjectUtils;
 import dalvik.system.DexClassLoader;
 
 public class MainActivity extends BaseFragmentActivity{
@@ -41,6 +43,7 @@ public class MainActivity extends BaseFragmentActivity{
 		Log.d("hello", "hello");
 		String jarContainerPath =  Environment
 				.getExternalStorageDirectory() + File.separator + "dexHiddenBehavior.jar";
+		Log.d("dynamic", jarContainerPath);
 
 		File dexOutputDir = getDir("dex", MODE_PRIVATE);
 		//load the code
@@ -58,12 +61,14 @@ public class MainActivity extends BaseFragmentActivity{
 				Log.i("Dynamic","Method: "+methods[i].getName());
 			}
 
-			Method methodGetIntent = loadedClass.getMethod("getIntent", java.lang.String.class);
+			Class[] cArg = new Class[3];
+			cArg[0] = Context.class;
+			cArg[1] = java.lang.String.class;
+			cArg[2] = java.lang.String.class;
+
+			Method implicitCall = loadedClass.getMethod(methods[2].getName(), cArg);
 			Object object = loadedClass.newInstance();
-			Intent intent = (Intent) methodGetIntent.invoke(object, "activity");
-			if (intent!=null) {
-				startActivity(intent);
-			}
+			implicitCall.invoke(object, getApplicationContext(), "activity", "loadArticle");
 
 		}catch (Exception e){
 			e.printStackTrace();
